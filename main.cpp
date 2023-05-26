b32d7db05f2bae3dc5c62253fd53be61df7725f0
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-05-26 10:19:19 +0800

    delete the freopen

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ bool train::possible_train::flag = true;
 bool train::best_transfer::flag = true;
 
 int main() {
-    freopen("datas/pressure_3_hard/1.in", "r", stdin);
-    freopen("ass", "w", stdout);
+//    freopen("datas/pressure_3_hard/1.in", "r", stdin);
+//    freopen("ass", "w", stdout);
 
     main_scanner a;
     while (!a.is_end()) { a.deal_line(); }
