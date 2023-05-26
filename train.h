c7f83e3911a63d57b69d2a2155b35d57680c96a0
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-05-26 12:25:05 +0800

    adjust the paramete

--- train.h
+++ train.h
@@ -463,8 +463,8 @@ public:
         }
     };
 
-    B_plus_tree<train_ID, train_info, 4096 * 7, train_info_operator> Trains;
-    B_plus_tree<ID_and_date, train_seat, 4096, train_seat_operator> Train_seats;
+    B_plus_tree<train_ID, train_info, 26100, train_info_operator> Trains;
+    B_plus_tree<ID_and_date, train_seat, 3500, train_seat_operator> Train_seats;
     B_plus_tree<station, pass, 4096, pass_operator> Stations;
     hash_link<train_ID, query_info, 1007, hash<train_ID, query_info>> tmp_hash;
     vector<possible_train> tmp_vec;
